7d61689e8ca1777daa29db88eac4c39eae9c0bb9
mohamed <mohamed2004hazem@gmail.com>
2025-03-14 17:52:16 +0200

    make the login function and handel the entries from the user

--- controller/c-auth.js
+++ controller/c-auth.js
@@ -4,9 +4,10 @@ const BadRequest = require('../errors/badReq')
 const bcrypt = require('bcryptjs')
 const { StatusCodes } = require('http-status-codes')
 const jwt = require('jsonwebtoken')
+const Unauthorized = require('../errors/unauth')
 class Authentication {
+    //register functin
     async register(req, res) {
-        const { name, email, password } = req.body;
         const vaildEmail = await userSchema.findOne({ email });
         if (vaildEmail) {
             throw new BadRequest('this email is already registerd!')
@@ -15,4 +16,23 @@ class Authentication {
         const token = userSchema.createJWT()
         res.status(StatusCodes.CREATED).json({ name: user.name, token })
     }
+    //login function
+    async login(req,res){
+        const {email , password} = req.body;
+        if (!email || !password) {
+            throw new BadRequest('please provid email and password')
+        }
+        const findEmail = await userSchema.findOne({ email })
+        if (!findEmail) {
+            throw new Unauthorized('Invalid credentials')
+        }
+        const isMatch = await userSchema.comparePassword(password)
+        if (!isMatch) {
+            throw new Unauthorized('Invalid credentials')
+        }
+        const token = userSchema.createJWT()
+        res.status(StatusCodes.OK).json({ name: user.name, token })
+    }
 }
+
+module.exports = Authentication;
