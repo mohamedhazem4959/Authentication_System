df4dde20a7d6814e14ea1666476e7338866b7b54
mohamed <mohamed2004hazem@gmail.com>
2025-03-14 17:51:11 +0200

    make compare method to compare the password

--- model/s-auth.js
+++ model/s-auth.js
@@ -39,4 +39,9 @@ authSchema.methods.createJWT = function(){
         }
     )
 }
+
+authSchema.methods.comparePassword = async function(password){
+    const isMatch = await bcrypt.compare(password,this.password)
+    return isMatch
+}
 module.exports = mongodb.model('users' , authSchema)
