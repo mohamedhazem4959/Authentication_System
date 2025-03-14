15eb27be0d8cd3111af4ac410ca64b30ecabea09
mohamed <mohamed2004hazem@gmail.com>
2025-03-14 17:57:45 +0200

    importing the c-auth that on the controller folder to use the register and login

--- routes/r-auth.js
+++ routes/r-auth.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const route = express.Router()
 
+const Authentication = require('../controller/c-auth.js')
 
-route.route('/register').post()
-route.route('login').post()
+route.route('/register').post(Authentication.register)
+route.route('login').post(Authentication.login)
 module.exports = route;
